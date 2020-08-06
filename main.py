a933922c250dfcc793c60b195f492a1c0fe53a6d
Gonzalo Benavides <mono@Pangui.local>
2020-08-06 14:06:40 -0400

    Added Flash and Firestore handlers. First GET calls implemented

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify, make_response
+from flask_restful import Resource, Api
+import firebase_admin
+from firebase_admin import credentials, firestore, initialize_app
+
+# Firestore DB initialization
+cred = credentials.Certificate("people-ae72e-firebase-adminsdk-pksjb-b9f891908b.json")
+default_app = initialize_app(cred)
+db = firestore.client()
+persons_ref = db.collection('persons')
+
+#Flask App initialization
+app = Flask(__name__)
+
+#API Handler initialization
+api = Api(app)
+
+#Resource class to handle calls passing national_id
+class Person(Resource):
+    def get(self,national_id):
+        person = persons_ref.document(national_id).get()
+        return make_response(jsonify(person.to_dict()), 200)
+
+#Resource class to handle calls not passing GET arguments, such as PUT, POST and get all      
+class PersonList(Resource):
+    def get(self):
+        persons = [doc.to_dict() for doc in persons_ref.stream()]
+        return make_response(jsonify(persons), 200)
+
+#Registering routes into the API handler    
+api.add_resource(Person,'/people/<id>')
+api.add_resource(PersonList,'/people')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
